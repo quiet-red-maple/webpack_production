2f43c035f19491b49c858d413a7bd64f6dd83174
郝泉宇 <13121800163@163.com>
2020-06-02 18:26:14 +0800

    生产环境配置

---
+++ webpack.config.js
@@ -0,0 +1,119 @@
+const { reslove } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const optimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+
+// 定义nodejs环境变量，决定使用browserslist的哪个环境
+process.env.NODE_ENV = 'production';
+
+const commonCssLoader = [
+  MiniCssExtractPlugin.loader,
+  'css-loader',
+  {
+    // 还需要在packagejson中browserslist
+    loader: 'postcss-loader',
+    options: {
+      ident: 'postcss',
+      plugins: () => [
+        require('postcss-preset-env')()
+      ]
+    }
+  },
+]
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: reslove(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          ...commonCssLoader
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          ...commonCssLoader,
+          'less-loader'
+        ]
+      },
+      // 当一个文件要被多个loader处理，那么一定要指定loader的先后顺序：
+      // 先执行eslint 再执行 babel
+      {
+        // package中eslintConfig配置 -->airbnb
+        test: /\.js$/,
+        exclude: /node_modules/,
+        // 优先执行
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        options: {
+          fix: true
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            {
+              // 按需加载
+              useBuiltIns: 'usage',
+              // 指定core-js版本
+              corejs: {
+                version: 3
+              },
+              targets: {
+                chrome: '60',
+                firefox: '60',
+                ie: '9',
+                safari: '10',
+                edge: '17'
+              }
+            }
+          ]
+        }
+      }, 
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8 * 1024,
+          name: '[hash:10].[ext]',
+          outputPath: 'imgs'
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        exclude: /\.(js|less|css|html|jpg|jpeg|png|gif)$/,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'media'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/built.css'
+    }),
+    new optimizeCssAssetsWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true
+      }
+    })
+  ],
+  mode: 'production'
+}
